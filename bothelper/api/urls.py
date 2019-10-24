0e28d12819460bbe4b832784dfa7b35d364658fc
Dightmerc <dightmerc@gmail.com>
2019-10-24 22:17:15 +0500

    Admin panel Refactoring | Agency added

--- bothelper/api/urls.py
+++ bothelper/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import StatView, OnlineStatView, OperationMoreView, OperationShowView, OperationView, OperationMoreViewCommon, OperationViewCommon, OperationShowViewCommon, InactiveCommonView, OnlineAccountView, OnlineMain
-
+from .views import OnlineStatViewAgencySingle, OperationShowViewCommonAgency, OperationMoreViewCommonAgency, OperationMoreViewAgency, OperationViewCommonAgency, OperationShowViewAgency, OperationViewAgency
 urlpatterns = [
     path("stat/", StatView, name="stat"),
     path("online/accounts/create/", OnlineAccountView, name="onlineStat"),
@@ -11,9 +11,28 @@ urlpatterns = [
     path("operation/<str:operation>/show/<int:pk>/", OperationShowView, name="onlineStatShow"),
     path("operation/common/<str:operation>/show/<int:pk>/", OperationShowViewCommon, name="onlineStatShowCommon"),
     path("announcement/<int:pk>/", OperationView, name="onlineStatOperation"),
-    path("common/<int:pk>/", OperationViewCommon, name="onlineStatOperation"),
-    path("common/<str:operation>/<str:property>/<int:pk>/", OperationMoreViewCommon, name="onlineStatOperation"),
-    path("inactive/order/<int:pk>/", InactiveCommonView, name="onlineStatOperation"),
+    path("common/<int:pk>/", OperationViewCommon, name="onlineStatOperation1"),
+    path("common/<str:operation>/<str:property>/<int:pk>/", OperationMoreViewCommon, name="onlineStatOperation2"),
+    path("inactive/order/<int:pk>/", InactiveCommonView, name="onlineStatOperation3"),
+
+    path("statistics/<int:pk>", OnlineStatViewAgencySingle, name="onlineStat"),
+    path("operation/common/agency/<str:operation>/show/<int:pk>/<int:telegram_user>", OperationShowViewCommonAgency, name="onlineStatShowCommonAgency"),
+    path("common/agency/<str:operation>/<str:property>/<int:pk>/<int:telegram_user>", OperationMoreViewCommonAgency, name="onlineStatOperationAgency"),
+    path("more/agency/<str:operation>/<str:property>/<int:pk>/<int:rieltor>", OperationMoreViewAgency, name="onlineStatMoreAgency"),
+    path("common/agency/<int:pk>/", OperationViewCommonAgency, name="onlineStatOperation4"),
+    path("agency/operation/<str:operation>/show/<int:pk>/<int:rieltor>", OperationShowViewAgency, name="onlineStatShowAgency"),
+
+    path("agency/announcement/<int:pk>/", OperationViewAgency, name="onlineStatOperation"),
+
+
+
+
+
+
+
+
+    
+
     
     # path("inactive/rieltororder/<int:pk>/", InactiveCommonView, name="onlineStatOperation"),
 
--- bothelper/api/views.py
+++ bothelper/api/views.py
@@ -77,6 +77,245 @@ def OnlineAccountView(request):
         return redirect("/accounts/login/")
 
 
+def OperationShowViewCommonAgency(request, operation, pk, telegram_user):
+    if request.user.is_authenticated:
+        if True:
+
+            paginator = Paginator(Order.objects.filter(active=True).filter(type=str(operation)).filter(user=TelegramUser.objects.get(pk=telegram_user)), 15)
+
+            page_count = []
+            a = 0
+            while a<paginator.num_pages:
+                a += 1
+                page_count.append(int(a))
+
+
+            if not pk in page_count:
+                return HttpResponse("Page not found", status=404)
+                
+            next_page = "#"
+            prev_page = "#"
+
+            if pk>1:
+                prev_page = str(pk - 1)
+            else:
+                prev_page = "#"
+
+            if pk + 1 > paginator.num_pages:
+                next_page = "#"
+            else:
+                next_page = str(pk + 1)
+
+
+            current_page = pk
+            return render(request, 'api/statisticsMoreCommonAgency.html', {
+                "orders": paginator.page(current_page).object_list,
+                "page_count":page_count, 
+                "current_page": current_page, 
+                "prev":prev_page, 
+                "next":next_page,
+            })
+        else:
+            return HttpResponse("Доступ запрещён. Свяжитесь с администратором")
+        
+    else:
+
+        return redirect("/accounts/login/")
+
+
+def OperationViewAgency(request, pk):
+    if request.user.is_authenticated:
+        if True:
+
+            order = OnlineRieltorOrder.objects.get(pk=pk)
+            location = f"{order.location_X} {order.location_X}"
+            data = []
+            data.append(order.type)
+            data.append(order.property)
+            data.append(order.title)
+            data.append(order.region)
+            data.append(order.reference)
+            data.append(location)
+            data.append(order.room_count)
+            data.append(order.square)
+            data.append(order.area)
+            data.append(order.state)
+            data.append(order.ammount)
+            data.append(order.add_info)
+            data.append(order.contact)
+            data.append(order.main_floor)
+            data.append(order.floor)
+            data.append(order.rieltor)
+            data.append(order.main_state)
+
+
+
+            text = OnlineGenerateEndText(data)
+
+            
+            return render(request, 'api/statisticsSingle.html', {
+                "order": order,
+                "text": text,
+                
+            })
+        else:
+            return HttpResponse("Доступ запрещён. Свяжитесь с администратором")
+    else:
+
+        return redirect("/accounts/login/")
+
+def OperationShowViewAgency(request, operation, pk, rieltor):
+    if request.user.is_authenticated:
+        if True:
+        
+            paginator = Paginator(OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=OnlineRieltor.objects.get(id=rieltor)).filter(type=str(operation)), 15)
+
+            page_count = []
+            a = 0
+            while a<paginator.num_pages:
+                a += 1
+                page_count.append(int(a))
+
+
+            if not pk in page_count:
+                return HttpResponse("Page not found", status=404)
+                
+            next_page = "#"
+            prev_page = "#"
+
+            if pk>1:
+                prev_page = str(pk - 1)
+            else:
+                prev_page = "#"
+
+            if pk + 1 > paginator.num_pages:
+                next_page = "#"
+            else:
+                next_page = str(pk + 1)
+
+
+            current_page = pk
+            return render(request, 'api/statisticsMoreAgency.html', {
+                "orders": paginator.page(current_page).object_list,
+                "page_count":page_count, 
+                "current_page": current_page, 
+                "prev":prev_page, 
+                "next":next_page,
+            })
+        else:
+            return HttpResponse("Доступ запрещён. Свяжитесь с администратором")
+        
+    else:
+
+        return redirect("/accounts/login/")
+
+    
+def OperationViewCommonAgency(request, pk):
+    if request.user.is_authenticated:
+        if True:
+            order = Order.objects.get(pk=pk)
+            location = f"{order.location_X} {order.location_X}"
+            data = []
+            data.append(order.type)
+            data.append(order.property)
+            data.append(order.title)
+            data.append(order.region)
+            data.append(order.reference)
+            data.append(location)
+            data.append(order.room_count)
+            data.append(order.square)
+            data.append(order.area)
+            data.append(order.state)
+            data.append(order.ammount)
+            data.append(order.add_info)
+            data.append(order.contact)
+            data.append(order.main_floor)
+            data.append(order.floor)
+
+
+
+            text = GenerateEndText(data)
+
+            
+            return render(request, 'api/statisticsSingle.html', {
+                "order": order,
+                "text": text,
+                
+            })
+        else:
+            return HttpResponse("Доступ запрещён. Свяжитесь с администратором")
+    else:
+
+        return redirect("/accounts/login/")
+
+
+def OnlineStatViewAgencySingle(request, pk):
+    week=date.today()-timedelta(days=7)
+
+    today = date.today()
+
+    rieltors = OnlineRieltor.objects.all()
+
+    max = 0
+    Top_rieltor = None
+    for rieltor in rieltors:
+        if OnlineRieltorOrder.objects.filter(rieltor=rieltor).count() > max:
+            max = OnlineRieltorOrder.objects.filter(rieltor=rieltor).count()
+            Top_rieltor = rieltor
+
+    selected_rieltor = OnlineRieltor.objects.get(pk=pk)
+    common_user = CommonRieltorUser.objects.get(rieltor=selected_rieltor)
+    telegram_user = TelegramUser.objects.get(phone=int(common_user.user.username))
+
+    
+    if request.user.is_authenticated:
+        # if CommonRieltorUser.objects.get(user__username=request.user).active:
+            
+
+        #     try:
+        #         rieltor = TelegramUser.objects.get(phone=request.user.3)
+        #     except Exception as e:
+        #         return HttpResponse("Доступ разрешен только для риелторов. Войдите в учетную запись риелтора")
+        if True:
+
+            return render(request, 'api/statistics2agency.html', {
+                "saleOrders": Order.objects.filter(active=True).filter(type="sale").filter(user=telegram_user),
+                "rentOrders": Order.objects.filter(active=True).filter(type="rent").filter(user=telegram_user),
+
+                "saleFlat": Order.objects.filter(active=True).filter(type="sale").filter(property="Квартира").filter(user=telegram_user),
+                "saleLand": Order.objects.filter(active=True).filter(type="sale").filter(property="Участок земли").filter(user=telegram_user),
+                "saleArea": Order.objects.filter(active=True).filter(type="sale").filter(property="Участок").filter(user=telegram_user),
+                "saleFreeArea": Order.objects.filter(active=True).filter(type="sale").filter(property="Коммерческая недвижимость").filter(user=telegram_user),
+
+                "rentFlat": Order.objects.filter(active=True).filter(type="rent").filter(property="Квартира").filter(user=telegram_user),
+                "rentLand": Order.objects.filter(active=True).filter(type="rent").filter(property="Участок земли").filter(user=telegram_user),
+                "rentArea": Order.objects.filter(active=True).filter(type="rent").filter(property="Участок").filter(user=telegram_user),
+                "rentFreeArea": Order.objects.filter(active=True).filter(type="rent").filter(property="Коммерческая недвижимость").filter(user=telegram_user),
+
+                "onlineRieltorSaleOrders": OnlineRieltorOrder.objects.filter(rieltor=selected_rieltor).filter(type="sale"),
+                "onlineRieltorRentOrders": OnlineRieltorOrder.objects.filter(rieltor=selected_rieltor).filter(type="rent"),
+
+                "onlineRieltor": selected_rieltor,
+                "telegram_user": telegram_user.id,
+
+                "onlineRieltoronlineRieltorsaleFlat": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="sale").filter(property="Квартира"),
+                "onlineRieltorsaleLand": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="sale").filter(property="Участок земли"),
+                "onlineRieltorsaleArea": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="sale").filter(property="Участок"),
+                "onlineRieltorsaleFreeArea": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="sale").filter(property="Коммерческая недвижимость"),
+
+                "onlineRieltorrentFlat": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="rent").filter(property="Квартира"),
+                "onlineRieltorrentLand": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="rent").filter(property="Участок земли"),
+                "onlineRieltorrentArea": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="rent").filter(property="Участок"),
+                "onlineRieltorrentFreeArea": OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=selected_rieltor).filter(type="rent").filter(property="Коммерческая недвижимость"),
+
+            })
+        else:
+            return HttpResponse("Доступ запрещён. Свяжитесь с администратором")
+    else:
+
+        return redirect("/accounts/login/")
+
+
 def OnlineAccountView(request):
 
     if request.user.is_authenticated:
@@ -84,6 +323,9 @@ def OnlineAccountView(request):
             form = PostForm(request.POST, request.FILES)
             if form.is_valid():
                 post = form.save()
+                post.user = request.user
+                post.save()
+
                 return redirect('/')
 
             else:
@@ -388,6 +630,93 @@ def OperationShowView(request, operation, pk):
 
         return redirect("/accounts/login/")
 
+def OperationMoreViewAgency(request, operation, property, pk, rieltor):
+    if request.user.is_authenticated:
+        if True:
+
+            paginator = Paginator(OnlineRieltorOrder.objects.filter(active=True).filter(rieltor=OnlineRieltor.objects.get(id=rieltor)).filter(type=str(operation)).filter(property=str(property)), 15)
+
+            page_count = []
+            a = 0
+            while a<paginator.num_pages:
+                a += 1
+                page_count.append(int(a))
+
+
+            if not pk in page_count:
+                return HttpResponse("Page not found", status=404)
+                
+            next_page = "#"
+            prev_page = "#"
+
+            if pk>1:
+                prev_page = str(pk - 1)
+            else:
+                prev_page = "#"
+
+            if pk + 1 > paginator.num_pages:
+                next_page = "#"
+            else:
+                next_page = str(pk + 1)
+
+
+            current_page = pk
+            return render(request, 'api/statisticsMore.html', {
+                "orders": paginator.page(current_page).object_list,
+                "page_count":page_count, 
+                "current_page": current_page, 
+                "prev":prev_page, 
+                "next":next_page,
+            })
+        else:
+            return HttpResponse("Доступ запрещён. Свяжитесь с администратором")
+    else:
+
+        return redirect("/accounts/login/")
+
+
+def OperationMoreViewCommonAgency(request, operation, property, pk, telegram_user):
+    if request.user.is_authenticated:
+        if True:
+            paginator = Paginator(Order.objects.filter(active=True).filter(type=str(operation)).filter(property=str(property)).filter(user=telegram_user), 15)
+
+            page_count = []
+            a = 0
+            while a<paginator.num_pages:
+                a += 1
+                page_count.append(int(a))
+
+
+            if not pk in page_count:
+                return HttpResponse("Page not found", status=404)
+                
+            next_page = "#"
+            prev_page = "#"
+
+            if pk>1:
+                prev_page = str(pk - 1)
+            else:
+                prev_page = "#"
+
+            if pk + 1 > paginator.num_pages:
+                next_page = "#"
+            else:
+                next_page = str(pk + 1)
+
+
+            current_page = pk
+            return render(request, 'api/statisticsMoreCommonAgency.html', {
+                "orders": paginator.page(current_page).object_list,
+                "page_count":page_count, 
+                "current_page": current_page, 
+                "prev":prev_page, 
+                "next":next_page,
+            })
+        else:
+            return HttpResponse("Доступ запрещён. Свяжитесь с администратором")
+    else:
+
+        return redirect("/accounts/login/")
 
 def OperationShowViewCommon(request, operation, pk):
     if request.user.is_authenticated:
